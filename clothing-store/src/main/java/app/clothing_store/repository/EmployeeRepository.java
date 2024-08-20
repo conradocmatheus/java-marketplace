42579e409e721904bbcc1a0ca9b083ed91fb946a
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-20 10:31:06 -0300

    Wrong typo, Changed EmployeeRepository to Employee

--- clothing-store/src/main/java/app/clothing_store/repository/EmployeeRepository.java
+++ clothing-store/src/main/java/app/clothing_store/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package app.clothing_store.repository;
 
+import app.clothing_store.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<EmployeeRepository, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
