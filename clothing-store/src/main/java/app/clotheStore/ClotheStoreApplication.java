e26e7e459d581e112334f0834aaf0f9d8a531829
conradocmatheus <conrado.matheus@protonmail.com>
2024-08-20 20:52:54 -0300

    Rename

--- clothing-store/src/main/java/app/clotheStore/ClotheStoreApplication.java
+++ clothing-store/src/main/java/app/clotheStore/ClotheStoreApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClothingStoreApplication {
+public class ClotheStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClothingStoreApplication.class, args);
+		SpringApplication.run(ClotheStoreApplication.class, args);
 	}
-
 }
