831b20a7bd2445b73cfe20d1c79e58ddf53c7625
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-06 15:04:31 -0300

    CustomerServiceTest update method

--- back-end/src/test/java/app/marketplace/service/CustomerServiceTest.java
+++ back-end/src/test/java/app/marketplace/service/CustomerServiceTest.java
@@ -12,9 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -39,15 +41,41 @@ public class CustomerServiceTest {
         customers.add(new Customer(5L, "Carlos Pereira", "56789012345", 35, "45995678901", Collections.emptyList()));
 
         when(customerRepository.findAll()).thenReturn(customers);
+
+        when(customerRepository.findById(3L)).thenReturn(Optional.of(customers.get(2)));
+
+        when(customerRepository.save(any(Customer.class))).thenAnswer(invocation -> invocation.getArgument(0));
     }
 
     @Test
     @DisplayName("Saving a customer")
     void saveCustomerTest() {
+
+        // Creates a new Customer
         Customer customer = new Customer(1L, "Joao pe", "15570528911", 19, "45991214710", Collections.emptyList());
         when(customerRepository.save(any(Customer.class))).thenReturn(customer);
         var result = customerService.save(customer);
         assertEquals(customer, result);
+    }
+
+    @Test
+    @DisplayName("Update a customer")
+    void updateCustomerTest() {
+        Customer updatedCustomer = new Customer();
+        updatedCustomer.setId(3L); // Existing client ID
+        updatedCustomer.setName("Pedro Santos Updated");
+        updatedCustomer.setCpf("34567890123");
+        updatedCustomer.setAge(41); // New age
+        updatedCustomer.setPhoneNumber("45993456789");
+        updatedCustomer.setSales(Collections.emptyList());
+
+        // Calls update method from service and assigns it to resulMessage
+        String resultMessage = customerService.update(updatedCustomer, 3L);
+
+        // Verifies if the message is the same as expected
+        assertEquals("Customer successfully updated", resultMessage);
+
+        verify(customerRepository).save(any(Customer.class));
     }
 
     @Test
