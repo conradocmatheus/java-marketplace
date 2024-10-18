78aad4b1d84b7ece425aa26ebf266a39625ae15d
conradocmatheus <conrado.matheus@protonmail.com>
2024-10-18 10:55:27 -0300

    basic navbar

--- front-end/src/app/components/layout/menu/menu.component.ts
+++ front-end/src/app/components/layout/menu/menu.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MdbCollapseModule, MdbDropdownModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
