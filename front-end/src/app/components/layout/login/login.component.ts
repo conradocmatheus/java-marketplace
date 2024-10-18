1268647c148ba9c07e2e9b1a4687702b6fe23ec0
conradocmatheus <conrado.matheus@protonmail.com>
2024-10-18 10:55:14 -0300

    login redirects to a page

--- front-end/src/app/components/layout/login/login.component.ts
+++ front-end/src/app/components/layout/login/login.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  user!: string;
+  password!: string;
+
+  router = inject(Router);
+
+  loginUser() {
+    if (this.user == 'admin' && this.password == 'admin') {
+      this.router.navigate(['admin/employee']);
+    } else {
+      alert('Incorrect credentials');
+    }
+  }
+}
